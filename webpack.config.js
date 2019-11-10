5b89cf051c8eafa6e67739ddf6d31996a827122a
Elias Aebi <elias.aebi@emineo.ch>
2019-11-10 08:42:23 +0100

    clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
   mode: 'development',
   plugins: [
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin(),
+    new CleanWebpackPlugin()
   ]
 }
